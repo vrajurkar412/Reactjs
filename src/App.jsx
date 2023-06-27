aa7a83d9ba0dcbf4cb9bacb75b2e06a56362d1de
vaibhavr <vrajurkar412@gmail.com>
2023-06-27 21:51:49 +0530

    2nd commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 // import logo from './logo.svg';
-import { Routes, Route, RouterProvider } from 'react-router-dom';
+// import { Routes, Route } from 'react-router-dom';
 import './App.css';
-import About from './Components/About';
+// import About from './Components/About';
 import { Alert } from './Components/Alert';
 import Navbar from './Components/Navbar';
 import TextForm from './Components/TextForm';
@@ -39,11 +39,12 @@ export default function App() {
     <>
       <Navbar title="textUtiles" aboutText="About us" mode={mode} toggleMode={toggleMode} />
       <Alert alert={alert} />
-      <Routes>
+      <TextForm heading="Enter the text analysis" mode={mode} toggleMode={toggleMode} showAlert={showAlert} />
+      {/* <Routes>
         <Route path='/' element={<TextForm heading="Enter the text analysis" mode={mode} toggleMode={toggleMode} showAlert={showAlert} />} />
         <Route path='/about' element={<About />} />
         {/* <About /> */}
-      </Routes>
+      {/* </Routes> */}
     </>
   )
 }
